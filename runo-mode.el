f5e493eb0b90a891e3b5b984338272244c2f9e1f
JuhisVain <Juho.Vainio@protonmail.ch>
2022-02-16 13:27:08 +0200

    Finnish syllabification function and test

--- runo-mode.el
+++ runo-mode.el
@@ -58,7 +58,76 @@
 	  )
     ))
 
-;;(runo-process-word "Laulaos")
+(defun runo-syllabificate (word)
+  "Return list of finnish syllables a single WORD."
+  (unless (zerop (length word))
+    (let* ((core-count (runo-tavu-ydin word))
+	   (end-count (runo-tavu-loppu (cl-subseq word core-count)))
+	   (end (+ core-count end-count)))
+      (cons (cl-subseq word 0 end)
+	    (runo-form-syllable (cl-subseq word end))))))
+
+(defun runo-tavu-ydin (word)
+  "Return the core vowels and preceeding consonants of the first syllable in WORD."
+  (cond ((string-match
+	  (rx (0+ (regex runo-consonant))
+	      (or (regex runo-diphtong)
+		  (regex runo-long-vowel)
+		  (regex runo-vowel)))
+	  word)
+	 (cadr (match-data)))
+	(t (error "No syllable core vowel found in \"%s\"" word))))
+
+(defun runo-tavu-loppu (word)
+  "Return count of consonants at end of syllable core, which has already been cut form WORD."
+  (cond
+   ((zerop (length word)) 0)
+   ((string-match runo-vowel (cl-subseq word 0 1))
+    0) ;; Vowel -> new syllable
+   ((= (length word) 1)
+    1) ;; Only one letter and it isn't a vowel
+   ((not (string-match runo-vowel word))
+    (length word)) ;; no vowels left
+   ((= (length word) 2) 0)
+   ((string-match (rx (regex runo-resonant-consonant)
+		      (regex runo-vowel))
+		  (cl-subseq word 0 3))
+    1) ;; Drop consonant & vowel
+   ((string-match runo-double-consonant
+		  (cl-subseq word 0 2))
+    1) ;; Drop duplicate consonant
+   ((string-match runo-resonant-double-consonant
+		  (cl-subseq word 0 3))
+    2) ;; Drop duplicate consonant
+   ((string-match (rx (1+ (regex runo-consonant))
+		      (regex runo-vowel))
+		  word)
+    (- (cadr (match-data)) 2)) ;; Drop consonant & vowel
+   ))
+
+(defun test ()
+  "Test syllabification."
+  (let ((poetry "
+\"Vait ole, mieletön, lunnaistas älä haastele mulle.
+Koittamaton kun viel' oli Patroklon tuhopäivä,
+heltyen helpommin minä iliolaisia säästin,
+vangiten mont' elävältä ja kauas kaupata antain;
+nytp' ei kuoloa ainoakaan ole karttava, jonka
+tuo kedol' Ilionin joku kuoloton kätteni valtaan
+sen asujoist', ei semminkään Priamon oma poika.
+Kuollos, ystävä, siis sinä myös! Älä ruikuta suotta!
+Kuolihan Patrokloskin, vaikk' urohomp' oli paljon.
+Katso'os, kuink' olen itse mä sankari sorja ja uljas,
+korkea mulla on taatto ja kuoloton kantaja kallis;
+vaan mua vartoelee toki kuolo ja mahtava Moira.
+Aamu se joutuva on tai ehtoo tai sydänpäivä,
+jolloin hengen vie joku multakin kamppaelussa
+iskien peitsellään tai jänteelt' ampuen nuolen.\"")
+	(syllables nil))
+    (dolist (word (split-string poetry "\\W" t))
+      (push (runo-syllabificate word) syllables))
+    (reverse syllables)))
+
 
 (provide 'runo-mode)
 ;;; runo-mode.el ends here
