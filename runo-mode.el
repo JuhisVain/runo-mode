730d4259731ce7248e5a2f4a47a0de9407d4c763
JuhisVain <Juho.Vainio@protonmail.ch>
2022-02-16 13:26:35 +0200

    Bunch more regexp statements

--- runo-mode.el
+++ runo-mode.el
@@ -20,17 +20,35 @@
 
 (defconst runo-vowel
   "[aeiouyäö]")
+(defconst runo-not-vowel
+  "[^aeiouyäö]")
 (defconst runo-long-vowel
   (concat "\\(?1:" runo-vowel "\\)\\1"))
 (defconst runo-consonant
   "[bcdfghjklmnpqrstvwxz]")
+(defconst runo-not-double-consonant
+  "\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1\\{0\\}")
+(defconst runo-double-consonant
+  "\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1")
 (defconst runo-resonant
-  "[rlmn]")
+  "\\(?1:[rlmn]\\)\\1\\{0\\}")
+
+(defconst runo-resonant-consonant
+  "[rlmn][bcdfghjkpqstvwxz]")
+(defconst runo-resonant-double-consonant
+  "[rlmn]\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1")
+
+(defconst runo-not-new-syllable
+  "[bcdfghjklmnpqrstvwxz]*\\(?:[bcdfghjklmnpqrstvwxz][aeiouyäö]\\)\\{0\\}")
+
+(defconst runo-voodoo
+  "\\(?:\\(?:[bcdfghjklmnpqrstvwxz][aeiouyäö]\\)\\{0\\}\\)\\|\\(?:\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1\\{0\\}\\)")
 
 (defun runo-process-word (word)
   "Split WORD into finnish syllables."
   (let* ((first-vowel-index (string-match-p runo-vowel word))
 	 (vowel-next (cl-subseq word first-vowel-index (+ 2 first-vowel-index))))
+    ;; Vowel length in first syllable:
     (cond ((string-match runo-diphtong vowel-next)
 	   'diphtongi)
 	  ((string-match runo-long-vowel vowel-next)
