c37828f3dad011dfa61d800cf047c88c4f1146dd
JuhisVain <Juho.Vainio@protonmail.ch>
2022-02-19 11:19:46 +0200

    Remove useless regexp vars

--- runo-mode.el
+++ runo-mode.el
@@ -20,29 +20,19 @@
 
 (defconst runo-vowel
   "[aeiouyäö]")
-(defconst runo-not-vowel
-  "[^aeiouyäö]")
 (defconst runo-long-vowel
   (concat "\\(?1:" runo-vowel "\\)\\1"))
 (defconst runo-consonant
   "[bcdfghjklmnpqrstvwxz]")
-(defconst runo-not-double-consonant
-  "\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1\\{0\\}")
 (defconst runo-double-consonant
   "\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1")
 (defconst runo-resonant
   "\\(?1:[rlmn]\\)\\1\\{0\\}")
-
 (defconst runo-resonant-consonant
   "[rlmn][bcdfghjkpqstvwxz]")
 (defconst runo-resonant-double-consonant
   "[rlmn]\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1")
 
-(defconst runo-not-new-syllable
-  "[bcdfghjklmnpqrstvwxz]*\\(?:[bcdfghjklmnpqrstvwxz][aeiouyäö]\\)\\{0\\}")
-
-(defconst runo-voodoo
-  "\\(?:\\(?:[bcdfghjklmnpqrstvwxz][aeiouyäö]\\)\\{0\\}\\)\\|\\(?:\\(?1:[bcdfghjklmnpqrstvwxz]\\)\\1\\{0\\}\\)")
 
 (defun runo-process-word (word)
   "Split WORD into finnish syllables."
